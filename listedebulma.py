ef7143882edaf1f056b309573f5b9ae9a24bb151
Semih <semihciplak@gmail.com>
2020-08-10 21:24:04 +0300

    listedeki elemanı sorgulama
    Bir liste var ve bir karakterin listede olup olmadığını sorguladığımız bir if ve else deneme programı

---
+++ listedebulma.py
@@ -0,0 +1,8 @@
+#Bir liste var ve bir karakterin listede olup olmadığını sorguladığımız bir if ve else deneme programı
+
+liste=["a","b","c","d"]
+hedef_harf= "x"
+
+if hedef_harf in liste:
+    print("Baba burada")
+else: print("Burada kimse yok")
