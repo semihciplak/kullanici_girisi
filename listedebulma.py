e68eae942291a15ab9f3a311cea0ce095d2b8ddb
Semih <semihciplak@gmail.com>
2020-08-10 21:35:11 +0300

    Update listedebulma.py
    kullanıcıya önce listeyi gösteriyorum ve kullanıcıya soruyorum, listeye eleman eklemek istiyor mu? eğer istiyorsan listeye ekleniyor. Eklemek istediği eleman listede varsa o zaman listede olduğunu söylüyor.

--- listedebulma.py
+++ listedebulma.py
@@ -1,8 +1,11 @@
 #Bir liste var ve bir karakterin listede olup olmadığını sorguladığımız bir if ve else deneme programı
 
 liste=["a","b","c","d"]
-hedef_harf= "x"
+hedef_harf=input("Eğer listeye olmayan bir eleman eklemek istiyorsan buraya yazabilirsin. ")
 
 if hedef_harf in liste:   #bu in özelliğini daha önce görmemiştim. "in" listenin içindekileri arama gibi düşünülebilir. 
-    print("Baba burada")
-else: print("Burada kimse yok")
+   print("yazdığın eleman zaman listede mevcut")
+    print(liste)
+else: 
+    liste.append(hedef_harf)
+    print("yazdığınız listede yoktu, ama ben onu listeye ekledim. Yeni listen", liste, "bu şekilde")
