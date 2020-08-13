3a3053af50907e5feda0f758db54be700d9297fa
Semih <semihciplak@gmail.com>
2020-08-13 13:33:45 +0300

    değişikliker yaptım
    extra güncellemeler yapmaya devam ediyor.

--- d.gaz fatura.py
+++ d.gaz fatura.py
@@ -28,6 +28,53 @@ while(yüzde!=100):
         
         if yüzde!=100:
                print("Uyarı toplam %100 olmalıdır.Sizin yüzdeniz",yüzde)
+        
+        
+        toplam=int(input("Doğalgaz faturanız kaç para:"))
+       
+    
+d1=int(input("Birinci daire faturanın yüzde kaçını ödüyor:"))	        
+d2=int(input("İkinci daire faturanın yüzde kaçını ödüyor:"))	        
+
+
+ekle = input("Hesaplanması için yeni daire eklemek ister misin? E/H:")
+
+if ekle == "E":
+    d3 = int(input("Yeni daire faturanın yüzde kaçını ödüyor:"))
+    ödeme1 = toplam*d1/100
+    ödeme2 = toplam*d2/100
+    ödeme3 = toplam*d3/100  
+    print("Birinci dairenin doğalgaz faturası", ödeme1)
+    print("İkinci dairenin doğalgaz faturası", ödeme2)
+    print("Üçüncü dairenin doğalgaz faturası", ödeme3)
+    
+    ekle1 = input("Hesaplaması için yeni daire eklemek ister misin? E/H:") 
+    if ekle1 == "E":
+        d4 = int(input("Yeni daire faturanın yüzde kaçını ödüyor:"))
+        ödeme1 - toplam*d1/100
+        ödeme2 - toplam*d2/100
+        ödeme3 - toplam*d3/100
+        ödeme4 - toplam*d4/100
+        print("Birinci dairenin doğalgaz faturası", ödeme1)
+        print("İkinci dairenin doğalgaz faturası", ödeme2)
+        print("Üçüncü dairenin doğalgaz faturası", ödeme3)
+        print("Dördüncü dairenin doğalgaz faturası", ödeme4)
+    elif ekle1 =="H":
+        ödeme1 - toplam*d1/100
+        ödeme2 - toplam*d1/100
+        ödeme3 - toplam*d1/100
+        print("Birinci dairenin doğalgaz faturası", ödeme1)
+        print("İkinci dairenin doğalgaz faturası", ödeme2)
+        print("Üçüncü dairenin doğalgaz faturası", ödeme3)
+        
+    
+        
+elif ekle == "H":
+    ödeme1=toplam*d1/100
+    ödeme2=toplam*d2/100
+    
+    print("Birinci dairenin doğalgaz faturası",ödeme1)
+    print("İkinci dairenin doğalgaz faturası",ödeme2)
 
 
 
